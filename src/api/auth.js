ddb6c804b842221c4441305415e6d38e6f2d2377
mcancetin <cmuhammed5300@gmail.com>
2024-03-13 17:00:22 +0300

    user access token functionality added

---
+++ src/api/auth.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+import open from "open";
+import http from "http";
+import url from "url";
+
+const TWITCH_AUTH_URL = "https://id.twitch.tv/oauth2/token";
+
+export default class Auth {
+	loginType = Object.freeze({
+		CLIENT_CREDENTIALS: "clientCredentials",
+		AUTHORIZATION_CODE: "authorizationCode"
+	});
+	constructor(clientId, clientSecret) {
+		this.clientId = clientId;
+		this.clientSecret = clientSecret;
+	}
+
+	async clientCredentials() {
+		try {
+			const response = await axios.post(TWITCH_AUTH_URL, {
+				client_id: this.clientId,
+				client_secret: this.clientSecret,
+				grant_type: "client_credentials"
+			});
+			console.log(response.data);
+			return {
+				access_token: response.data.access_token,
+				refres_token: null
+			};
+		} catch (error) {
+			throw new Error("Login failed:", error);
+		}
+	}
+
+	async authorizationCode({ scope = "" }) {
+		if (!scope) throw new Error("Scope is required");
+		const code = new Promise(async (resolve, reject) => {
+			try {
+				const server = http.createServer((request, response) => {
+					const queryObject = url.parse(request.url, true).query;
+
+					resolve(queryObject.code);
+					server.close();
+					response.end("You can now close this tab!");
+				});
+
+				server.listen(80);
+
+				const authUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${this.clientId}&redirect_uri=http://localhost&response_type=code&scope=${scope}`;
+				await open(authUrl);
+			} catch (error) {
+				reject(error);
+			}
+		});
+
+		const response = await axios.post(TWITCH_AUTH_URL, {
+			client_id: this.clientId,
+			client_secret: this.clientSecret,
+			grant_type: "authorization_code",
+			code: await code,
+			redirect_uri: "http://localhost"
+		});
+
+		console.log(response.data);
+
+		return {
+			access_token: response.data.access_token,
+			refresh_token: response.data.refresh_token
+		};
+	}
+
+	async refreshToken() {
+		// ... (refresh token logic)
+	}
+}
--- src/api/client.js
+++ src/api/client.js
@@ -1,39 +1,59 @@
 import axios from "axios";
+
+import Auth from "./auth.js";
 import User from "./user.js";
 import Clip from "./clip.js";
 
-const BASE_URL = "https://api.twitch.tv/helix";
-const AUTH_URL = "https://id.twitch.tv/oauth2/token";
+const TWITCH_BASE_URL = "https://api.twitch.tv/helix";
 
 export default class Client {
-	constructor(client_id, client_secret) {
-		this.client_id = client_id;
-		this.client_secret = client_secret;
-		this.instance = axios.create();
+	constructor(clientId, clientSecret) {
+		this.authService = new Auth(clientId, clientSecret);
+		this.axiosInstance = axios.create({
+			baseURL: TWITCH_BASE_URL,
+			headers: {
+				"Client-Id": this.clientId
+			}
+		});
+
+		this.axiosInstance.interceptors.response.use(
+			(response) => response.data,
+			async (error) => {
+				if (error.response.status === 401) {
+					await this.authService.refreshToken();
+					return this.axiosInstance(error.config);
+				}
+				return Promise.reject(error);
+			}
+		);
 	}
 
-	async login() {
-		try {
-			const res = await axios.post(AUTH_URL, {
-				client_id: this.client_id,
-				client_secret: this.client_secret,
-				grant_type: "client_credentials"
-			});
-
-			this.instance.defaults.baseURL = BASE_URL;
-			this.instance.defaults.headers.common["Client-Id"] = this.client_id;
-			this.instance.defaults.headers.common[
-				"Authorization"
-			] = `Bearer ${res.data.access_token}`;
-
-			this._initServices();
-		} catch (error) {
-			console.error("Login failed:", error);
-		}
+	// method can be "clientCredentials" or "authorizationCode". Default method is client credentials. If method is "authorizationCode", scope is required
+	async login({
+		method = this.authService.loginType.CLIENT_CREDENTIALS,
+		scope = ""
+	}) {
+		if (method === this.authService.loginType.AUTHORIZATION_CODE && !scope)
+			throw new Error("Scope is required");
+		const { accessToken, refreshToken } = await this.authService[method]({
+			scope
+		});
+		this.#setAuthHeaders(accessToken, refreshToken);
+		this.#initServices();
+		console.log("Logged in");
 	}
 
-	_initServices() {
-		this.userService = new User(this.instance);
-		this.clipService = new Clip(this.instance);
+	#setAuthHeaders(access_token, refresh_token = null) {
+		this.access_token = access_token;
+		this.refresh_token = refresh_token;
+
+		this.axiosInstance.defaults.headers.common[
+			"Authorization"
+		] = `Bearer ${access_token}`;
+	}
+
+	#initServices() {
+		this.userService = new User(this.axiosInstance);
+		this.clipService = new Clip(this.axiosInstance);
 	}
 }
