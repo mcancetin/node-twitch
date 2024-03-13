793c956346a7e6eb4b5cc022e13f7b3361ce7b54
mcancetin <cmuhammed5300@gmail.com>
2024-03-13 17:01:14 +0300

    Remove unnecessary code for fetching user clips

--- main.js
+++ main.js
@@ -4,10 +4,3 @@ const client = new Client(
 	"dea4e38o9b01z0hxj5fujulh64ehid",
 	"zpcizsrdg0qgo50dfsflavkdzzi1vo"
 );
-await client.login();
-
-const { userService, clipService } = client;
-
-const user = await userService.getUser("wtcn");
-const clips = await clipService.getClips({ broadcaster_id: user.id });
-console.log(clips);
