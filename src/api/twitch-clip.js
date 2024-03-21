4d9e96e62fb67c3defa3c0a2dc97e52fb8e8fef9
mcancetin <cmuhammed5300@gmail.com>
2024-03-21 23:39:38 +0300

    folder renamed

--- src/api/twitch-clip.js
+++ src/api/twitch-clip.js
@@ -1,5 +1,7 @@
 import { createSearchParams } from "../utils/createSearchParams.js";
 
+import puppeteer from "puppeteer";
+
 const searchParams = {
 	broadcaster_id: "",
 	game_id: "",
@@ -13,6 +15,7 @@ const searchParams = {
 
 export default class Clip {
 	#axiosInstance;
+	clips = [];
 	constructor(axiosInstance) {
 		this.#axiosInstance = axiosInstance;
 	}
@@ -27,9 +30,30 @@ export default class Clip {
 			if (response.data.length === 0) {
 				throw new Error("There is no clip with the given parameters");
 			}
-			return response.data;
+			this.clips = response.data;
+			return this.clips;
 		} catch (error) {
 			throw new Error(`Error while fetching clip: ${error.message}`);
 		}
+	}
+
+	async getClipsUrl() {
+		const urls = [];
+
+		for (const clip of this.clips) {
+			console.log("Getting clip url for", clip.title);
+			const src = await puppeteer.launch().then(async (browser) => {
+				const page = await browser.newPage();
+				await page.goto(clip.url);
+				const src = await page.evaluate(
+					() => document.querySelector("video").src
+				);
+				await browser.close();
+				return src;
+			});
+			urls.push({ ...clip, url: src });
+		}
+
+		return urls;
 	}
 }
